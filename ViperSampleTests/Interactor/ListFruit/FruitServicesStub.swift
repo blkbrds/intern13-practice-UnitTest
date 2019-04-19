04c6c1c9c097a91395514667125a34debf1ade81
Tan Huynh T <tan.huynh@asiantech.vn>
2019-04-19 14:55:17 +0700

    Test ListFruitInteractor

---
+++ ViperSampleTests/Interactor/ListFruit/FruitServicesStub.swift
@@ -0,0 +1,37 @@
+//
+//  FruitServicesStub.swift
+//  ViperSampleTests
+//
+//  Created by MBA0010 on 4/19/19.
+//  Copyright © 2019 Van Le H. All rights reserved.
+//
+
+import Foundation
+
+@testable import ViperSample
+
+final class FruitServicesStub: FruitServices {
+    
+    var completionFlag: Bool
+    
+    init(completionFlag: Bool) {
+        self.completionFlag = completionFlag
+    }
+
+    func getFruits(completion: @escaping (Result<Array<FruitEntity>>) -> ()) {
+        if completionFlag {
+            completion(.success(Dummy.fruits))
+        } else {
+            completion(.failure(Dummy.error))
+        }
+    }
+}
+
+extension FruitServicesStub {
+    
+    struct Dummy {
+        static let fruits = [FruitEntity(name: "WaterMelon", price: 15), FruitEntity(name: "StarFruit", price: 300)]
+        
+        static let error = NSError(domain: "https://404Error", code: 404, userInfo: nil)
+    }
+}
