ebdac24faca2b00f5fc222a7dd4d82732bf53e96
Que Dinh T <que.dinh@asiantech.vn>
2019-05-02 10:21:59 +0700

    remove space line

--- TreeView/QueDinh/TreeView/TreeView/AppDelegate.swift
+++ TreeView/QueDinh/TreeView/TreeView/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         let navigationController = UINavigationController(rootViewController: HomeViewController())
         window = UIWindow(frame: UIScreen.main.bounds)
